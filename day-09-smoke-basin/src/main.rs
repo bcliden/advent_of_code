13ba574f8f3b46ae58285590e72cc26438e28d5b
Benjamin Liden <benjamin.c.liden@gmail.com>
2021-12-11 18:25:33 -0500

    feat: Day 9 --completed-- 🎉

--- day-09-smoke-basin/src/main.rs
+++ day-09-smoke-basin/src/main.rs
@@ -5,118 +5,117 @@ type Floor = Vec<Vec<usize>>;
 type VisitorLog = HashSet<Coords>;
 type LowestLog = Vec<(Coords, usize)>;
 
-fn get_square_value<'a> ((row, col): &Coords, floor: &'a Floor) -> Option<&'a usize>  {
+fn get_square_value<'a>((row, col): &Coords, floor: &'a Floor) -> Option<&'a usize> {
     floor
         .get(row.clone() as usize)
         .map(|_row| _row.get(col.clone() as usize))
         .flatten()
 }
 
-fn visit(floor: &Floor, visited: &mut VisitorLog, lowest_points: &mut LowestLog, own_coord: Coords) {
-    if visited.contains(&own_coord) {
-        return;
-    }
-
-
-
-    let (row, col) = own_coord;
-    let own_value= get_square_value(&own_coord, floor).map(|v| v.clone()).unwrap_or(9);
-
-    // println!("[i] Visiting {:?} -> {}", (row, col), own_value);
-    visited.insert((row, col));
-
-    let all_coords = vec![
+fn neighboring_squares((row, col): Coords) -> Vec<Coords> {
+    vec![
         (row, col - 1), // top
         (row, col + 1), // bottom
         (row - 1, col), // left
         (row + 1, col), // right
-    ];
+    ]
+}
 
-    let coords_pairs: Vec<(Coords, usize)> = all_coords
-        .into_iter()
+fn get_values_for(coords: &Vec<Coords>, floor: &Floor) -> Vec<(Coords, usize)> {
+    coords
+        .iter()
+        .cloned()
         // get value for next squares
         .map(|c| (c, get_square_value(&c, floor)))
         // filter out missing ones
         .filter(|(_, val)| val.is_some())
         .map(|(c, v)| (c, v.map(|v| v.clone()).unwrap_or(9)))
-        .collect();
+        .collect()
+}
 
-    let is_least = &own_value < coords_pairs.iter().map(|(_, v)| v).min().unwrap();
-
-    // let lower_pairs: Vec<_> = coords_pairs
-    //     .iter().cloned()
-    //     .filter(|(_other_coord, other_val)| {
-    //         // println!(
-    //         //     "\t[i] comparing current {:?}[{}] to next {:?}[{}] and returning lower={}",
-    //         //     own_coord,
-    //         //     &own_value,
-    //         //     other_coord,
-    //         //     other_val,
-    //         //     other_val < &own_value
-    //         // );
-    //         /*   9
-    //             999
-    //              9    should the center be lower? no. current val (9) should be LESS THAN other val
-    //         */    
-    //         // in which our current cell is strictly LESS THAN the other cell
-    //         &own_value > other_val
-    //     })
-    //     .collect();
-    // let higher_pairs: Vec<_> = coords_pairs
-    //     .iter().cloned()
-    //     .filter(|(_other_coord, other_val)| {
-    //         // println!(
-    //         //     "\t[i] comparing current {:?}[{}] to next {:?}[{}] and returning lower={}",
-    //         //     own_coord,
-    //         //     &own_value,
-    //         //     other_coord,
-    //         //     other_val,
-    //         //     other_val < &own_value
-    //         // );
-    //         // in which our current cell is strictly LESS THAN the other cell
-    //         &own_value < other_val
-    //     })
-    //     .collect();
-    // println!("[i] For {:?}[{}], found ", own_coord, own_value);
-    // println!("\t[i] Higher pairs: {:?}", higher_pairs);
-    // println!("\t[i] Lower pairs: {:?}", lower_pairs);
-    // if lower_pairs.is_empty() {
-    if is_least {
-        // lowest_points.insert(own_coord);
-        // or just add the value? would need a signature change
-        // println!("[!] lowest area detected: {:?} -> {}", own_coord, own_value);
-        lowest_points.push((own_coord, own_value));
+fn visit(
+    floor: &Floor,
+    visited: &mut VisitorLog,
+    lowest_points: &mut LowestLog,
+    own_coord: Coords,
+) {
+    if visited.contains(&own_coord) {
+        return;
     }
 
-    // Turns out, I didn't need to recurse like this... who'd a thunk it?
+    let (row, col) = own_coord;
+    let own_value = get_square_value(&own_coord, floor)
+        .map(|v| v.clone())
+        .unwrap_or(9);
 
-    // } else {
-    //     // visit any NON visited lower pairs
-    //     let non_visited_pairs: Vec<_> = coords_pairs
-    //         .into_iter()
-    //         .filter(|(c, _)| !visited.contains(c))
-    //         .collect();
-    //     for (coord, _value) in non_visited_pairs {
-    //         visit(floor, visited, lowest_points, coord);
-    //     }
-    // }
+    visited.insert((row, col));
+
+    let all_coords = neighboring_squares(own_coord.clone());
+    let coords_pairs = get_values_for(&all_coords, floor);
+
+    let is_least = &own_value < coords_pairs.iter().map(|(_, v)| v).min().unwrap();
+    if is_least {
+        lowest_points.push((own_coord, own_value));
+    }
+}
+
+/// Recursively mark current square and neighboring squares.
+/// Records visited squares and squares within current basin,
+/// finally returning the size of the basin.
+fn mark_squares(
+    floor: &Floor,
+    visited: &mut VisitorLog,
+    current_basin: &mut VisitorLog,
+    own_coord: Coords,
+) {
+    if visited.contains(&own_coord) {
+        return;
+    }
+    visited.insert(own_coord.clone());
+
+    match get_square_value(&own_coord, floor) {
+        Some(n) if *n == 9 => return, // quit if 9
+        Some(_) => current_basin.insert(own_coord.clone()),
+        None => return, // quit if None
+    };
+
+    neighboring_squares(own_coord)
+        .into_iter()
+        .for_each(|c| mark_squares(floor, visited, current_basin, c));
+}
+
+fn get_basin_size(floor: &Floor, own_coord: Coords) -> usize {
+    let mut visited: VisitorLog = HashSet::new(); // per-basin-search level
+    let mut current_basin: VisitorLog = HashSet::new();
+
+    mark_squares(floor, &mut visited, &mut current_basin, own_coord);
+
+    current_basin.iter().count()
 }
 
 fn does_part2(input: &str) -> usize {
     let floor: Floor = parse_input(input);
     let mut visited: VisitorLog = HashSet::new();
-    let mut lowest_point_values: LowestLog = vec![];
+    let mut basin_origins: LowestLog = vec![];
 
-    // just recursively do it
-    for (row, _line) in floor.iter().enumerate() {
-        for (col, _value) in floor[row].iter().enumerate() {
-            visit(&floor, &mut visited, &mut lowest_point_values, (row as i32, col as i32));
+    for (row, _) in floor.iter().enumerate() {
+        for (col, _) in floor[row].iter().enumerate() {
+            visit(
+                &floor,
+                &mut visited,
+                &mut &mut basin_origins,
+                (row as i32, col as i32),
+            );
         }
     }
 
-    println!("Basins located at: {:?}", lowest_point_values);
-
-    0
+    let mut basin_sizes: Vec<_> = basin_origins
+        .into_iter()
+        .map(|(c, _)| c) // drop the values, we don't need 'em
+        .map(|c| get_basin_size(&floor, c))
+        .collect();
+    basin_sizes.sort(); // just reverse iterate below instead of sorting DESC
+    basin_sizes.into_iter().rev().take(3).fold(1, |acc, n| acc * n)
 }
 
 fn does_part1(input: &str) -> usize {
@@ -124,14 +123,16 @@ fn does_part1(input: &str) -> usize {
     let mut visited: VisitorLog = HashSet::new();
     let mut lowest_point_values: LowestLog = vec![];
 
-    // just recursively do it
     for (row, _line) in floor.iter().enumerate() {
         for (col, _value) in floor[row].iter().enumerate() {
-            visit(&floor, &mut visited, &mut lowest_point_values, (row as i32, col as i32));
+            visit(
+                &floor,
+                &mut visited,
+                &mut lowest_point_values,
+                (row as i32, col as i32),
+            );
         }
     }
-
-    println!("All lowest points: {:?}", lowest_point_values);
 
     lowest_point_values.into_iter().map(|(_, v)| v + 1).sum()
 }
@@ -146,7 +147,8 @@ fn parse_input(input: &str) -> Floor {
     }
 
     for (n, line) in input.lines().enumerate() {
-        floor[n] = line.trim()
+        floor[n] = line
+            .trim()
             .chars()
             .map(|c| c.to_digit(10).unwrap() as usize)
             .collect();
@@ -157,12 +159,12 @@ fn parse_input(input: &str) -> Floor {
 
 fn main() {
     const FULL: &str = include_str!("../input.txt");
-    
+
     let total_risk = does_part1(FULL);
     println!("Total floor risk: {}", total_risk);
 
     let basin_sizes = does_part2(FULL);
-    assert_eq!(basin_sizes, 496);
+    println!("total basin factor calculation: {}", basin_sizes);
 }
 
 #[cfg(test)]
@@ -186,7 +188,7 @@ mod tests {
     fn part1_full() {
         let total_risk = does_part1(FULL);
         /*
-            Wrong answers discovered: 
+            Wrong answers discovered:
             - 1716
         */
         assert_eq!(total_risk, 496);
@@ -195,16 +197,16 @@ mod tests {
     #[test]
     fn part2_example() {
         let total_risk = does_part2(EXAMPLE);
-        assert_eq!(total_risk, 15);
+        assert_eq!(total_risk, 1134);
     }
 
     #[test]
     fn part2_full() {
         let total_risk = does_part2(FULL);
         /*
-            Wrong answers discovered: 
-            - 
+            Wrong answers discovered:
+            -
         */
-        assert_eq!(total_risk, 496);
+        assert_eq!(total_risk, 902880);
     }
 }
