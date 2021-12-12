1a18336b475c612ff697b2f090ff5ade4b0a5793
Benjamin Liden <benjamin.c.liden@gmail.com>
2021-12-12 13:24:19 -0500

    feat: add const maps for brace pairs, solve part 2, refactoring

--- day-10-syntax-scoring/src/main.rs
+++ day-10-syntax-scoring/src/main.rs
@@ -1,11 +1,32 @@
+use phf::phf_map;
 use std::collections::{HashMap, VecDeque};
 
-#[derive(Debug, PartialEq, Eq)]
-enum LineStatus {
+enum Status {
     Complete,
     Incomplete,
     Illegal(char),
 }
+
+struct Line {
+    chars_left: VecDeque<char>,
+    status: Status,
+}
+
+// compile-time const map
+const CLOSE_BRACE_TO_OPEN: phf::Map<char, char> = phf_map! {
+    ')' => '(',
+    ']'=> '[' ,
+    '}'=> '{',
+    '>'=> '<',
+};
+
+// compile-time const map
+const OPEN_BRACE_TO_CLOSED: phf::Map<char, char> = phf_map! {
+     '('  => ')',
+     '[' => ']' ,
+     '{' => '}',
+     '<' => '>',
+};
 
 /// Compare enums by category, not by value
 fn variant_eq<T>(a: &T, b: &T) -> bool {
@@ -16,30 +37,25 @@ fn parse_input(input: &str) -> Vec<VecDeque<char>> {
     input.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn part_one(input: &str) -> usize {
-    let scores: HashMap<char, usize> =
-        HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)]);
-    let closing_pairs: HashMap<char, char> =
-        HashMap::from([(')', '('), (']', '['), ('}', '{'), ('>', '<')]);
-
-    let mut results: Vec<LineStatus> = vec![];
-    let data = parse_input(input);
-
+fn get_solutions(data: Vec<VecDeque<char>>) -> Vec<Line> {
+    let mut results: Vec<Line> = vec![];
     for mut line in data {
         let mut queue: VecDeque<char> = VecDeque::new();
         loop
-        /* chars in line */
+        /* through chars in line */
         {
-            // println!("Reading line {:?}", line);
-            // println!("With queue of {:?}", queue);
             let ch = line.pop_front();
-            // println!("Just popped char {:?}", ch);
-
             if ch.is_none() {
                 if queue.is_empty() {
-                    results.push(LineStatus::Complete);
+                    results.push(Line {
+                        chars_left: queue,
+                        status: Status::Complete,
+                    });
                 } else {
-                    results.push(LineStatus::Incomplete);
+                    results.push(Line {
+                        chars_left: queue,
+                        status: Status::Incomplete,
+                    });
                 }
                 break; // exit reading line
             }
@@ -51,34 +67,46 @@ fn part_one(input: &str) -> usize {
                 '{' => queue.push_back(ch),
                 '<' => queue.push_back(ch),
                 ')' => {
-                    if queue.back() == Some(&closing_pairs[&ch]) {
-                        queue.pop_back(); // dump the last item
+                    if queue.back() == Some(&CLOSE_BRACE_TO_OPEN[&ch]) {
+                        queue.pop_back(); // remove the paired char
                     } else {
-                        results.push(LineStatus::Illegal(ch));
+                        results.push(Line {
+                            chars_left: queue,
+                            status: Status::Illegal(ch),
+                        });
                         break; // exit reading line
                     }
                 }
                 ']' => {
-                    if queue.back() == Some(&closing_pairs[&ch]) {
+                    if queue.back() == Some(&CLOSE_BRACE_TO_OPEN[&ch]) {
                         queue.pop_back(); // remove the paired char
                     } else {
-                        results.push(LineStatus::Illegal(ch));
+                        results.push(Line {
+                            chars_left: queue,
+                            status: Status::Illegal(ch),
+                        });
                         break; // exit reading line
                     }
                 }
                 '}' => {
-                    if queue.back() == Some(&closing_pairs[&ch]) {
+                    if queue.back() == Some(&CLOSE_BRACE_TO_OPEN[&ch]) {
                         queue.pop_back(); // remove the paired char
                     } else {
-                        results.push(LineStatus::Illegal(ch));
+                        results.push(Line {
+                            chars_left: queue,
+                            status: Status::Illegal(ch),
+                        });
                         break; // exit reading line
                     }
                 }
                 '>' => {
-                    if queue.back() == Some(&closing_pairs[&ch]) {
+                    if queue.back() == Some(&CLOSE_BRACE_TO_OPEN[&ch]) {
                         queue.pop_back(); // remove the paired char
                     } else {
-                        results.push(LineStatus::Illegal(ch));
+                        results.push(Line {
+                            chars_left: queue,
+                            status: Status::Illegal(ch),
+                        });
                         break; // exit reading line
                     }
                 }
@@ -86,25 +114,81 @@ fn part_one(input: &str) -> usize {
             }
         }
     }
-
     results
+}
+
+fn part_one(input: &str) -> usize {
+    let point_values: HashMap<char, usize> =
+        HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)]);
+
+    let data = parse_input(input);
+    let mut solutions = get_solutions(data);
+
+    // retain ONLY illegal lines
+    solutions.retain(|line| variant_eq(&line.status, &Status::Illegal('_')));
+
+    solutions
         .into_iter()
-        .filter(|r| variant_eq(r, &LineStatus::Illegal('_')))
-        .map(|r| {
-            if let LineStatus::Illegal(n) = r {
+        .map(|line| {
+            if let Status::Illegal(n) = line.status {
                 return n; // get the value from the enum!
             } else {
                 unreachable!();
             }
         })
-        .map(|c| scores.get(&c).map(|n| n.clone()).unwrap_or(0)) // get score from dict
+        .map(|c| {
+            point_values
+                .get(&c) // get point value from dictionary
+                .map(|n| n.clone())
+                .unwrap_or(0)
+        })
         .sum()
+}
+
+fn part_two(input: &str) -> usize {
+    let point_values: HashMap<char, usize> =
+        HashMap::from([(')', 1), (']', 2), ('}', 3), ('>', 4)]);
+    let data = parse_input(input);
+    let mut results = get_solutions(data);
+
+    // retain ONLY incomplete lines
+    results.retain(|line| variant_eq(&line.status, &Status::Incomplete));
+
+    let solutions: Vec<Vec<char>> = results
+        .iter()
+        .map(|line| {
+            line
+                .chars_left
+                .iter()
+                .map(|c| OPEN_BRACE_TO_CLOSED[c])
+                .rev()
+                .collect()
+        })
+        .collect();
+
+    let scores: Vec<Vec<usize>> = solutions
+        .into_iter()
+        .map(|chars| chars.into_iter().map(|c| point_values[&c]).collect())
+        .collect();
+
+    let mut calculated_scores: Vec<usize> = scores
+        .into_iter()
+        .map(|scores_per_vec| {
+            scores_per_vec
+                .into_iter()
+                .fold(0, |acc_score, next_score| (acc_score * 5) + next_score)
+        })
+        .collect();
+    calculated_scores.sort();
+    calculated_scores[calculated_scores.len() / 2] // the median score. thanks integer arithmetic!
 }
 
 fn main() {
     const FULL: &str = include_str!("../input.txt");
     let score: usize = part_one(FULL);
     println!("Score for part one: {}", score);
+    let score: usize = part_two(FULL);
+    println!("Score for part two: {}", score);
 }
 
 #[cfg(test)]
@@ -133,5 +217,17 @@ mod tests {
     fn does_part_one_full() {
         let result = part_one(FULL);
         assert_eq!(result, 315693);
+    }
+
+    #[test]
+    fn does_part_two_example() {
+        let result = part_two(EXAMPLE);
+        assert_eq!(result, 288957);
+    }
+
+    #[test]
+    fn does_part_two_full() {
+        let result = part_two(FULL);
+        assert_eq!(result, 1870887234);
     }
 }
