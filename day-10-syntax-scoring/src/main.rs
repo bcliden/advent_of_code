8fd7a0db24ba28dbc8d0085df6114474f4ca6bd7
Benjamin Liden <benjamin.c.liden@gmail.com>
2021-12-11 23:29:33 -0500

    feat: Day 10, part one complete!

---
+++ day-10-syntax-scoring/src/main.rs
@@ -0,0 +1,136 @@
+use std::collections::{HashMap, VecDeque};
+
+#[derive(Debug, PartialEq, Eq)]
+enum LineStatus {
+    Complete,
+    Incomplete,
+    Illegal(char),
+}
+
+/// Compare enums by category, not by value
+fn variant_eq<T>(a: &T, b: &T) -> bool {
+    std::mem::discriminant(a) == std::mem::discriminant(b)
+}
+
+fn parse_input(input: &str) -> Vec<VecDeque<char>> {
+    input.lines().map(|line| line.chars().collect()).collect()
+}
+
+fn part_one(input: &str) -> usize {
+    let scores: HashMap<char, usize> =
+        HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)]);
+    // let pairs: HashMap<char, char> =
+    //     HashMap::from([('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]);
+
+    let mut results: Vec<LineStatus> = vec![];
+    let data = parse_input(input);
+
+    for mut line in data {
+        let mut queue: VecDeque<char> = VecDeque::new();
+        loop
+        /* chars in line */
+        {
+            // println!("Reading line {:?}", line);
+            // println!("With queue of {:?}", queue);
+            let ch = line.pop_front();
+            // println!("Just popped char {:?}", ch);
+            if ch.is_none() {
+                if queue.is_empty() {
+                    results.push(LineStatus::Complete);
+                } else {
+                    results.push(LineStatus::Incomplete);
+                }
+                break; // exit reading line
+            }
+
+            let real_ch = ch.unwrap();
+            match real_ch {
+                '(' => queue.push_back(real_ch),
+                '[' => queue.push_back(real_ch),
+                '{' => queue.push_back(real_ch),
+                '<' => queue.push_back(real_ch),
+                ')' => {
+                    if queue.back() == Some(&'(') {
+                        queue.pop_back(); // dump the last item
+                    } else {
+                        results.push(LineStatus::Illegal(real_ch));
+                        break; // exit reading line
+                    }
+                }
+                ']' => {
+                    if queue.back() == Some(&'[') {
+                        queue.pop_back(); // remove the paired char
+                    } else {
+                        results.push(LineStatus::Illegal(real_ch));
+                        break; // exit reading line
+                    }
+                }
+                '}' => {
+                    if queue.back() == Some(&'{') {
+                        queue.pop_back(); // remove the paired char
+                    } else {
+                        results.push(LineStatus::Illegal(real_ch));
+                        break; // exit reading line
+                    }
+                }
+                '>' => {
+                    if queue.back() == Some(&'<') {
+                        queue.pop_back(); // remove the paired char
+                    } else {
+                        results.push(LineStatus::Illegal(real_ch));
+                        break; // exit reading line
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+    println!("Results: {:?}", results);
+    results
+        .into_iter()
+        .filter(|r| variant_eq(r, &LineStatus::Illegal('_')))
+        .map(|r| {
+            if let LineStatus::Illegal(n) = r {
+                return n; // get the value from the enum!
+            } else {
+                unreachable!();
+            }
+        })
+        .map(|c| scores.get(&c).map(|n| n.clone()).unwrap_or(0)) // get score from dict
+        .sum()
+}
+
+fn main() {
+    const FULL: &str = include_str!("../input.txt");
+    let score: usize = part_one(FULL);
+    println!("Score for part one: {}", score);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = r#"[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"#;
+    const FULL: &str = include_str!("../input.txt");
+
+    #[test]
+    fn does_part_one_example() {
+        let result = part_one(EXAMPLE);
+        assert_eq!(result, 26397);
+    }
+
+    #[test]
+    fn does_part_one_full() {
+        let result = part_one(FULL);
+        assert_eq!(result, 315693);
+    }
+}
